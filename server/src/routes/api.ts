695f84157ee4e66053108ef7237350e1d55289b1
Raj <raj.pulapakura@gmail.com>
2024-11-23 17:11:55 +1100

    Comment out authMiddleware

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -3,7 +3,7 @@ import authMiddleware from '../middleware/authMiddleware';
 
 export const apiRouter = express.Router();
 
-apiRouter.use(authMiddleware)
+// apiRouter.use(authMiddleware)
 
 apiRouter.get("/", async (req: Request, res: Response) => {
     res.send("Hello World");
